a1847d90809c84723036e7851afb02b6aed3c44a
panditdhamdhere <dhamdherepandit@gmail.com>
2024-12-12 00:20:17 +0530

    working on smart contracts

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LandingPage = () => {
-    return <div>landing page</div>;
+  return <div>landing page</div>;
 };
 
 export default LandingPage;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+const fs = require("fs");
+const privateKey = fs.readFileSync("secret.txt").toString();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {
+      chainId: 4202,
+    },
+    RootstockTestnet: {
+      url: "https://public-node.testnet.rsk.co",
+      accounts: [privateKey],
+      gasPrice: 100000000,
+      chainId: 31,
+    },
+  },
   solidity: "0.8.24",
+  allowUnlimitedContractSize: true,
+  throwOnTransactionFailures: true,
+  throwOnCallFailures: true,
+  loggingEnabled: true,
 };
